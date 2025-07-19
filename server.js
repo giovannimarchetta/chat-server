e70d3c3e438beebb4445eff2c8187c454ab289f6
giovanni_marchetta <giovanni.marchetta.25@gmail.com>
2025-07-19 20:50:18 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,28 +1,45 @@
-const PORT = process.env.PORT || 3000;
+const express = require('express');
+const http = require('http');
+const cors = require('cors');
+const { Server } = require('socket.io');
 
-const io = require("socket.io")(PORT, {
-	cors: {
-		origin: "*", // permetti connessioni da qualsiasi origine
-		methods: ["GET", "POST"]
-	}
+const app = express();
+const server = http.createServer(app);
+
+app.use(cors({
+  origin: "http://frisiawards.altervista.org", // consenti il tuo frontend Altervista
+  methods: ["GET", "POST"]
+}));
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://frisiawards.altervista.org",
+    methods: ["GET", "POST"]
+  }
 });
 
 const users = {};
 
 io.on('connection', socket => {
-	socket.on('new-user', name => {
-		users[socket.id] = name;
-		socket.broadcast.emit('user-connected', name);
-	});
-	
-	socket.on('send-chat-message', message => {
-		socket.broadcast.emit('chat-message', {message: message, name: users[socket.id] });
-	});
-	
-	socket.on('disconnect', () => {
-		socket.broadcast.emit('user-disconnected', users[socket.id]);
-		delete users[socket.id];
-	});
-	
-	console.log("Nuovo utente connesso");
+  socket.on('new-user', name => {
+    users[socket.id] = name;
+    socket.broadcast.emit('user-connected', name);
+  });
+
+  socket.on('send-chat-message', message => {
+    socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] });
+  });
+
+  socket.on('disconnect', () => {
+    socket.broadcast.emit('user-disconnected', users[socket.id]);
+    delete users[socket.id];
+  });
+
+  console.log("Nuovo utente connesso");
 });
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server in ascolto sulla porta ${PORT}`);
+});
+
